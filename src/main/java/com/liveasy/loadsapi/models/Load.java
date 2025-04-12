1571adaf74409432f99e03fbc68a2131f768be15
Rishabh <140707297+rishabhrawat05@users.noreply.github.com>
2025-04-12 10:46:36 +0530

    Create Load.java

---
+++ src/main/java/com/liveasy/loadsapi/models/Load.java
@@ -0,0 +1,93 @@
+package com.liveasy.loadsapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "loads")
+public class Load {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String loadType;
+    private Double weight;
+    private String origin;
+    private String destination;
+    private String status;
+
+    public Load() {
+    }
+
+    public Load(String loadType, Double weight, String origin, String destination, String status) {
+        this.loadType = loadType;
+        this.weight = weight;
+        this.origin = origin;
+        this.destination = destination;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLoadType() {
+        return loadType;
+    }
+
+    public void setLoadType(String loadType) {
+        this.loadType = loadType;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Load{" +
+                "id=" + id +
+                ", loadType='" + loadType + '\'' +
+                ", weight=" + weight +
+                ", origin='" + origin + '\'' +
+                ", destination='" + destination + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
